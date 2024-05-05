dc438f6ac31eb8cf3f7c0d4fed820fc8c0a34267
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2024-05-05 22:03:41 +0300

    fixed quaternion from euler

--- gimbal-main/src/lib/src/Quaternion.cpp
+++ gimbal-main/src/lib/src/Quaternion.cpp
@@ -41,19 +41,19 @@ Vector3<float, uint8_t> Quaternion::toEuler() const {
     return {{yaw}, {pitch}, {roll}};
 }
 
-Quaternion fromEuler(float yaw, float pitch, float roll) {
-    float cy = std::cos(yaw * 0.5);
+Quaternion Quaternion::fromEuler(float yaw, float pitch, float roll) {
     float sy = std::sin(yaw * 0.5);
-    float cp = std::cos(pitch * 0.5);
+    float cy = std::cos(yaw * 0.5);
     float sp = std::sin(pitch * 0.5);
-    float cr = std::cos(roll * 0.5);
+    float cp = std::cos(pitch * 0.5);
     float sr = std::sin(roll * 0.5);
+    float cr = std::cos(roll * 0.5);
 
     return Quaternion(
-        cy * cp * cr + sy * sp * sr,
-        sy * cp * cr - cy * sp * sr,
+        sy * sp * sr + cy * cp * cr,
+        cy * cp * sr - sy * sp * cr,
         cy * sp * cr + sy * cp * sr,
-        cy * cp * sr - sy * sp * cr
+        sy * cp * cr - cy * sp * sr
     );
 }
 
