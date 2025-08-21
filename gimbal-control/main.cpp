b9eaa1bd761f9eda25a29fb1b4f1c32a6e107602
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2025-08-21 19:44:20 +0200

    updated IK for correct angles

--- gimbal-control/main.cpp
+++ gimbal-control/main.cpp
@@ -9,7 +9,6 @@ constexpr static float    motorScalingFactor {2048 / F_PI};
 constexpr static float    intScalingFactor {32768 / F_PI};
 constexpr static float    ATT_LSB {10430.0f};
 constexpr static float    joystickFactor {F_DEG_TO_RAD * (F_PI / 100.0f)};
-constexpr static float    sqrt2 = sqrtf(2);
 constexpr static uint16_t deltaTime {1000 / updateRate};
 constexpr static float    maxVelocityPerStep {maxRestoringVelocity / updateRate};
 
@@ -251,27 +250,49 @@ Vector3<float, uint8_t> calculateAngles(const Vector3<float, uint8_t>& eulerAngl
 	float sinG = std::sin(g);
 
 	float cos2A = cosA * cosA;
-	float sin2B = sinB * sinB;
 	float cos2B = cosB * cosB;
 	float sin2G = sinG * sinG;
 
-	float sinM2 = sqrt2 * sinB;
-
-	auto signG {util::sign(util::abs(g) - F_PI_2)};
-
+	/* All this IK math takes about 500us of CPU time at 48MHz.
+	 * This is not great but it still comfortably fits within the 10ms time budget
+	 * and even one tick.
+	 * Still, these formulas come straight from MATLAB and probably can (and should)
+	 * be optimized further if possible
+	 */
 	return {
-	  {-2
-	   * atan2f(
-	       (sqrt2 * (sqrt2 * sinB - util::sign(a) * sqrtf(2 - 2 * cos2A * cos2B - 2 * sin2B))),
-	       (2 * (sqrtf(1 - 2 * sin2B) + cosA * cosB))
-	   )},
-	  {asinf(sinM2)},
-	  {F_PI_4
-	   + signG * 2
-	         * atan2f(
-	             (std::sqrt(2 - 2 * cos2B * sin2G - 2 * sin2B) + signG * 1),
-	             (sqrtf(1 - 2 * sin2B) - sqrt2 * cosB * sinG)
-	         )}
+	  {a < 0
+	       ? -2
+	             * atanf(
+	                 (0.5f
+	                  * (4.6188f * sinB
+	                     + 1.4142f * sqrtf(32.536f - 32 * cos2A * cos2B - 32 * powf(sinB - 0.12941f, 2) - 8.2822f * sinB)
+	                     - 2.3909f))
+	                 / (3.8637f * sqrtf(1 - 1.4291f * pow(sinB - 0.12941f, 2)) + 4 * cosA * cosB)
+	             )
+	       : -2
+	             * atanf(
+	                 -(0.5f
+	                   * (1.4142f * sqrtf(32.536f - 32 * cos2A * cos2B - 32 * powf(sinB - 0.12941f, 2) - 8.2822f * sinB)
+	                      - 4.6188f * sinB + 2.3909f))
+	                 / (3.8637f * sqrtf(1 - 1.4291f * powf(sinB - 0.12941f, 2)) + 4 * cosA * cosB)
+	             )},
+	  {asinf(1.1954f * sinB - 0.1547f)},
+	  {util::abs(g) > F_PI_2
+	       ? 2
+	             * atanf(
+	                 (0.25f
+	                  * (1.4142f * sqrtf(32.536f - 32 * cos2B * sin2G - 32 * powf(sinB - 0.12941f, 2) - 8.2822f * sinB)
+	                     - 2.1436f * sinB + 4.1411f))
+	                 / (1.7321f * sqrtf(1 - 1.4291f * powf(sinB - 0.12941f, 2)) - 2 * cosB * sinG)
+	             )
+	       : 2
+	             * atanf(
+	                 -(0.25f
+	                   * (2.1436f * sinB
+	                      + 1.4142f * sqrtf(32.536f - 32 * cos2B * sin2G - 32 * powf(sinB - 0.12941f, 2) - 8.2822f * sinB)
+	                      - 4.1411f))
+	                 / (1.7321f * sqrtf(1 - 1.4291f * powf(sinB - 0.12941f, 2)) - 2 * cosB * sinG)
+	             )}
 	};
 }
 
